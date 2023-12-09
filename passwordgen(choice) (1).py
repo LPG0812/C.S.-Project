0a521e38c60c5982019f75fa14e298d2e318e1d9
LPG0812 <151669554+LPG0812@users.noreply.github.com>
2023-12-09 16:49:23 +0530

    Update passwordgen(choice) (1).py

--- passwordgen(choice) (1).py
+++ passwordgen(choice) (1).py
@@ -1,25 +1,31 @@
 import random
-letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-number='0123456789'
-symbols='\'!@#$%^&*()_+}{|:"?><-=[]\;/.,'
-pw=''
-
-sym=int(input('Enter amount of symbols in the password:'))
-for i in range(sym):
-    pw+=(symbols[random.randrange(len(symbols))])
-
-n=int(input("Enter amount of numbers in password:"))
-for i in range(n):
-    pw+=(number[random.randrange(len(number))])
-
-let=int(input("Enter amount of letters in your password:"))
-for i in range(let):
-    pw+=(letters[random.randrange(len(letters))])
-
-
-
-if let+n+sym<4:
-    print('The length of the password is too short:')
-else:
-    a=''.join(random.sample(pw,len(pw)))
-    print(a)
+print('Welcome to our password generator!')
+while True:
+    letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    number='0123456789'
+    symbols='\'!@#$%^&*()_+}{|:"?><-=[]\;/.,'
+    pw=''
+    
+    sym=int(input('Enter amount of symbols in the password:'))
+    for i in range(sym):
+        pw+=(symbols[random.randrange(len(symbols))])
+    
+    n=int(input("Enter amount of numbers in password:"))
+    for i in range(n):
+        pw+=(number[random.randrange(len(number))])
+    
+    let=int(input("Enter amount of letters in your password:"))
+    for i in range(let):
+        pw+=(letters[random.randrange(len(letters))])
+    
+    
+    
+    if let+n+sym<4:
+        print('The length of the password is too short:')
+    else:
+        a=''.join(random.sample(pw,len(pw)))
+        print(a)
+    repeat=input('Would you like to try again? Yy/Nn')
+    if repeat in 'Nn':
+        print('Thank you for using our password generator!')
+        break
